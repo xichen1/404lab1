6028aebbfbdf8906bff4143e04be10e3494380a8
xichen1 <xichen.pan@gmail.com>
2021-09-07 23:01:42 -0400

    download the script itself and print the code out

--- pythonScript.py
+++ pythonScript.py
@@ -2,12 +2,17 @@ import requests
 
 def main():
     print(requests.__version__ + "\n")
-    
-    googleHome = requests.get("http://www.google.com")
+
     print("Requesting to GET the Google homepage...")
+    googleHome = requests.get("http://www.google.com")
     print("This is the headers of response:")
     print(googleHome.headers)
     print()
+
+    print("Downoading the script itself from github...")
+    scriptFile = requests.get("https://raw.githubusercontent.com/xichen1/404lab1/master/pythonScript.py")
+    print("This is the source code of the script file:")
+    print(scriptFile.text)
 
 
 if __name__ == "__main__":
