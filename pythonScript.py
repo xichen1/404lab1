4bb932118c6a077b2c4e94d10fe30a4dc9da28bc
xichen1 <xichen.pan@gmail.com>
2021-09-07 22:37:57 -0400

    get google homepage

--- pythonScript.py
+++ pythonScript.py
@@ -1,7 +1,14 @@
 import requests
 
 def main():
-    print(requests.__version__)
+    print(requests.__version__ + "\n")
+    
+    googleHome = requests.get("http://www.google.com")
+    print("Requesting to GET the Google homepage...")
+    print("This is the headers of response:")
+    print(googleHome.headers)
+    print()
+
 
 if __name__ == "__main__":
     main();
