464ccf1c8c71abaff5fa636a85e86fa4caa6de49
XichenPan <xichen.pan@gmail.com>
2021-09-10 13:52:53 -0600

    Add comment, remove semicolon

--- pythonScript.py
+++ pythonScript.py
@@ -1,14 +1,17 @@
 import requests
 
 def main():
+    # requests version
     print(requests.__version__ + "\n")
 
+    # GET google homepage
     print("Requesting to GET the Google homepage...")
     googleHome = requests.get("http://www.google.com")
     print("This is the headers of response:")
     print(googleHome.headers)
     print()
 
+    # download and print the file itself
     print("Downoading the script itself from github...")
     scriptFile = requests.get("https://raw.githubusercontent.com/xichen1/404lab1/master/pythonScript.py")
     print("This is the source code of the script file:")
@@ -16,4 +19,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main();
+    main()
